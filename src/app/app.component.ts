67891f57973bd6cef40193f1d522294f2be454e0
SreejaBiswas <bsreeja2002@gmail.com>
2024-02-05 19:24:48 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,5 +9,6 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'sreeja_angular';
+  name="sreeja";
+  framework="angular";
 }
